4419ce1bda96b40604f28953b6f6b51ccb03eeea
ashwani <pandafan949@gmail.com>
2023-04-21 16:10:26 +0530

    laravel crud

---
+++ laravelcrud.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3308
+-- Generation Time: Apr 21, 2023 at 12:34 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `laravelcrud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table laravelcrud.failed_jobs: #1932 - Table 'laravelcrud.failed_jobs' doesn't exist in engine
+-- Error reading data for table laravelcrud.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `laravelcrud`.`failed_jobs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laravel_task`
+--
+-- Error reading structure for table laravelcrud.laravel_task: #1932 - Table 'laravelcrud.laravel_task' doesn't exist in engine
+-- Error reading data for table laravelcrud.laravel_task: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `laravelcrud`.`laravel_task`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table laravelcrud.migrations: #1932 - Table 'laravelcrud.migrations' doesn't exist in engine
+-- Error reading data for table laravelcrud.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `laravelcrud`.`migrations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+-- Error reading structure for table laravelcrud.password_resets: #1932 - Table 'laravelcrud.password_resets' doesn't exist in engine
+-- Error reading data for table laravelcrud.password_resets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `laravelcrud`.`password_resets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table laravelcrud.personal_access_tokens: #1932 - Table 'laravelcrud.personal_access_tokens' doesn't exist in engine
+-- Error reading data for table laravelcrud.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `laravelcrud`.`personal_access_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table laravelcrud.users: #1932 - Table 'laravelcrud.users' doesn't exist in engine
+-- Error reading data for table laravelcrud.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `laravelcrud`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
